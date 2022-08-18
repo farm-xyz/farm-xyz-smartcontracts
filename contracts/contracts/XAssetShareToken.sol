b1640051c4c862052b54179082b4301be42f2a23
Alex Mincu <alex@mountsoftware.ro>
2022-08-19 02:47:41 +0300

    made XAssetShareToken.sol upgradable

--- contracts/contracts/XAssetShareToken.sol
+++ contracts/contracts/XAssetShareToken.sol
@@ -1,21 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "hardhat/console.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-abstract contract XAssetShareToken is ERC20, Ownable {
-    using SafeERC20 for IERC20;
+abstract contract XAssetShareToken is ERC20PermitUpgradeable, OwnableUpgradeable {
 
-    uint256 public totalValueLocked = 0;
+    function initialize(string memory name_, string memory symbol_) initializer public {
+        __ERC20_init(name_, symbol_);
+        __ERC20Permit_init(name_);
+    }
 
     // Ownable de catre XAssetBase
     // XAssetBase trebuie sa poata chema mint si burn
     // de implementat mint si burn ca public functions care sunt owner only
 
-    // notes @Florin: modifier `public` sau `internal` ?
+    // notes @Florin: modifier `public` sau `internal` ? Pimp: public is fine, internal ar fi accesibila doar din
+    // contract
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
